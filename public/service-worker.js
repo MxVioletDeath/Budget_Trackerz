e704f5ab5cce6f45f42e7d2b48f498c33858c499
Violet Death <ladymariaownsbro@gmail.com>
2020-07-17 20:51:14 -0500

    commit #2

---
+++ public/service-worker.js
@@ -0,0 +1,72 @@
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/db.js",
+    "/index.js",
+    "/manifest.json",
+    "/styles.css",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png",
+  ];
+  
+  
+  
+  self.addEventListener("install", event => {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+  });
+  
+  // The activate handler takes care of cleaning up old caches.
+//   self.addEventListener("activate", event => {
+//     const currentCaches = [PRECACHE, RUNTIME];
+//     event.waitUntil(
+//       caches.keys().then(cacheNames => {
+//         return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+//       }).then(cachesToDelete => {
+//         return Promise.all(cachesToDelete.map(cacheToDelete => {
+//           return caches.delete(cacheToDelete);
+//         }));
+//       }).then(() => self.clients.claim())
+//     );
+//   });
+  
+self.addEventListener("fetch", function(event) {
+    // cache all get requests to /api routes
+    if (event.request.url.includes("/api/")) {
+      event.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(event.request)
+            .then(response => {
+             
+              if (response.status === 200) {
+                cache.put(event.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch(err => {
+              
+              return cache.match(event.request);
+            });
+        }).catch(err => console.log(err))
+      );
+      return;
+    }
+    event.respondWith(
+      fetch(event.request).catch(function() {
+        return caches.match(event.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (event.request.headers.get("accept").includes("text/html")) {
+           
+            return caches.match("/");
+          }
+        });
+      })
+    );
+  });
